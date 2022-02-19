202c63fd03df176cca677573f6148a554d60031b
Erwin Rudi <erwin.rudi@rwth-aachen.de>
2022-02-19 15:27:50 +0100

    deploy heroku app

--- portfolio_app/static/js/app.js
+++ portfolio_app/static/js/app.js
@@ -2,13 +2,12 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
-const txta = document.getElementsByTagName('textarea');
+const txta = document.getElementsByTagName("textarea");
 const addTagBtn = document.querySelector("#add-btn");
 const blogArticle = document.querySelector(".blog-article");
 const expandSearchBtn = document.querySelector(".expand-search-btn");
 const contactBtn = document.querySelector(".hero-btn");
 const flashMessages = document.querySelector(".flash");
-
 
 // show sidebar
 navBtn.addEventListener("click", function() {
@@ -22,20 +21,22 @@ date.innerHTML = new Date().getFullYear();
 
 // add resizable textarea
 for (let i = 0; i < txta.length; i++) {
-  txta[i].setAttribute('style', 'height:' + (txta[i].scrollHeight) +
-    'px;overflow-y:hidden;');
+  txta[i].setAttribute(
+    "style",
+    "height:" + txta[i].scrollHeight + "px;overflow-y:hidden;"
+  );
   txta[i].addEventListener("input", OnInput, false);
 }
 
 function OnInput() {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
 }
 
 if (contactBtn) {
   const contactInfos = document.querySelectorAll(".contact-info");
   contactBtn.addEventListener("click", () => {
-    contactInfos.forEach((info) => {
+    contactInfos.forEach(info => {
       info.classList.add("show-contact");
       setTimeout(() => {
         info.classList.remove("show-contact");
@@ -49,14 +50,13 @@ function filesize(elem) {
   document.cookie = `filesize=${elem.files[0].size}`;
 }
 
-
 // add and delete tags
 if (addTagBtn) {
   const deleteTagBtn = document.querySelector(".delete-tag-btn");
   const articleTags = document.querySelector(".article-tags");
   const tagText = document.querySelector(".tag-text");
   addTagBtn.addEventListener("click", function() {
-    let input = document.createElement('input');
+    let input = document.createElement("input");
     input.type = "text";
     input.name = `tag${articleTags.childNodes.length}`;
     input.classList.add("create-tag");
@@ -75,10 +75,10 @@ if (addTagBtn) {
   });
 
   // show image name for image input in create_article
-  const input = document.querySelector('.hidden-btn');
-  const selectedImage = document.querySelector('.selected-image');
+  const input = document.querySelector(".hidden-btn");
+  const selectedImage = document.querySelector(".selected-image");
 
-  input.addEventListener('change', function(e) {
+  input.addEventListener("change", function(e) {
     let fileName = e.target.value.split("\\").pop();
 
     selectedImage.innerText = fileName;
@@ -126,12 +126,11 @@ if (expandSearchBtn) {
         searchContainer.classList.toggle("z-index");
       }, 300);
     }
-
   });
 
   const dropdownBtns = document.querySelectorAll("#dropdown-btn");
 
-  dropdownBtns.forEach((btn) => {
+  dropdownBtns.forEach(btn => {
     const dropdownContent = btn.nextElementSibling;
     btn.addEventListener("click", () => {
       dropdownContent.classList.toggle("show");
@@ -147,32 +146,38 @@ if (expandSearchBtn) {
   });
 }
 
-
-
 const projectsCenter = document.querySelector(".projects-center");
 
 async function ajaxUser() {
   const base = "https://api.github.com/users/";
   const clientID = "4425ed62040a0d1ace3b";
   const clientSecret = "fe313c4b30f340241ddab787a77f2116f861f732";
-  const user = "solinuck";
+  const user = "erwinru";
   // repos url
-  const reposURL =
-    `${base}${user}/repos?client_id="${clientID}"&client_secret="${clientSecret}"`;
+  const reposURL = `${base}${user}/repos?client_id="${clientID}"&client_secret="${clientSecret}"`;
 
   // get repos
   const reposData = await fetch(reposURL, {
     headers: {
-      'accept': 'application/vnd.github.mercy-preview+json'
+      accept: "application/vnd.github.mercy-preview+json"
     }
   });
   const repos = await reposData.json();
+  console.log(repos);
 
-  return repos;
+  const repoNames = [
+    "a-star-visualization",
+    "chess-engine",
+    "computational-physics",
+    "portfolio"
+  ];
+
+  return repos.filter(repo => repoNames.includes(repo.name));
 }
 
 function displayRepos(repos) {
   repos.forEach((repo, i) => {
+    console.log(repo);
     const a = document.createElement("a");
     a.classList.add(`project-${i + 1}`);
     a.href = `${repo.html_url}`;
@@ -181,10 +186,9 @@ function displayRepos(repos) {
     repo.topics.forEach(topic => {
       topicsString += `<span class="tag">${topic}</span>`;
     });
-    a.innerHTML =
-      `
+    a.innerHTML = `
     <article class="project">
-      <img src="https://raw.githubusercontent.com/solinuck/${repo.name}/master/img/readme-example.png" alt="single project" class="project-img">
+      <img src="https://raw.githubusercontent.com/erwinru/${repo.name}/master/img/readme-example.png" alt="single project" class="project-img">
       <div class="project-info">
         <h4>${repo.name}</h4>
         <p>${repo.description}</p>
@@ -199,6 +203,7 @@ function displayRepos(repos) {
 }
 
 if (projectsCenter) {
+  console.log(projectsCenter);
   ajaxUser().then(data => displayRepos(data));
 }
 
