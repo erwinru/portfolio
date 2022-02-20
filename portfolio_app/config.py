f34c35b8f1850095be7b19ddff959b44e1d58370
Erwin Rudi <erwin.rudi@gmail.com>
2022-02-20 13:35:38 +0100

    retrieve config from environment variables

--- portfolio_app/config.py
+++ portfolio_app/config.py
@@ -1,19 +1,15 @@
-import json
-
-with open("/etc/portfolio_config.json") as config_file:
-    config = json.load(config_file)
-
+import os
 
 class Config(object):
     DEBUG = False
     TESTING = False
-    GITHUB_TOKEN = config.get("GITHUB_TOKEN")
-    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
+    GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
-    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 class DevelopmentConfig(Config):
